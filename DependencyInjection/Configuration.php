846e7474e1cf06f8bf0287dcbd6dd90af47a967a
Jacek Łoziński <Jacek.lozinski@tmsolution.pl>
2015-12-09 09:09:25 +0100

    Merge origin/master
    Conflicts:
    	README.md

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,7 +17,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('sms');
+        $rootNode = $treeBuilder->root('tm_solution_sms');
 
         $rootNode
             ->children()
